a71adc3a7c31b0354422f376728048693e532ccf
Isaac <isaac@knightmoves.org>
2025-07-31 11:43:41 -0500

    test

--- HomeEnergyApi/Models/IWriteRepository.cs
+++ HomeEnergyApi/Models/IWriteRepository.cs
@@ -2,7 +2,7 @@ using System.Numerics;
 
 namespace HomeEnergyApi.Models
 {
-    public interface IWriteRepository<TId, T>
+    public interface IWriteRepository<TId, T> where TId : INumber<TId>
     {
         T Save(T entity);
         T Update(TId id, T entity);
